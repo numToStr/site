24cf7ae98c169d144a5ee0633b59079249515686
vkasraj <vikasraj1911@gmail.com>
2019-06-12 16:13:13 +0530

    config: added some plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/, // See below to configure properly
+        },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-no-sourcemaps",
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

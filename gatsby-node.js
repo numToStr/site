f86b97e31b71a9d643579bc931dbe8a7797d3130
vkasraj <vikasraj1911@gmail.com>
2019-06-20 19:23:40 +0530

    config: changed gql playground

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const dotenv = require("dotenv");
 
-// You can delete this file if you're not using it
+dotenv.config({
+    path: `.env.development`,
+});

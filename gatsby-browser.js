42217393acd11a8145d42aaf569c55b54a7dba7b
vkasraj <vikasraj1911@gmail.com>
2019-06-22 20:11:39 +0530

    blog: added syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,5 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+require("./src/styles/dracula-prism.css");
 
-// You can delete this file if you're not using it
 const React = require("react");
 const TopLayout = require("./src/components/TopLayout").default;
 const Layout = require("./src/components/Layout").default;
